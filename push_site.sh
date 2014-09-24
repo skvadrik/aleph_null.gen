802de5b8144437024f6d8664bc8c6bb5763484f8
Ulya Trofimovich <skvadrik@gmail.com>
2014-09-24 14:11:19 +0100

    push_site.sh: must use 'gh-pages' branch.

--- push_site.sh
+++ push_site.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -x
+#!/bin/sh
 
 set -e
 
@@ -7,8 +7,9 @@ commit_id=`git log -1 --format=format:"%h: %s (%ai)"`
 cd _site
 if [ ! -d .git ]; then
     git init
+    git checkout --orphan gh-pages
     git remote add --fetch origin git@github.com:skvadrik/aleph_null.git
 fi
 git add .
 git commit -s -m "sync skvadrik.github.io/aleph_null.git:${commit_id}" .
-git push --force -u origin master
+git push --force -u origin gh-pages
