37cab82674ae94116d1627a8a59295188bbca16b
Sergei Trofimovich <slyich@gmail.com>
2022-07-31 13:12:15 +0100

    push_site.sh: speed up push by reusing local object cache if exists

--- push_site.sh
+++ push_site.sh
@@ -7,6 +7,16 @@ commit_id=`git log -1 --format=format:"%h: %s (%ai)"`
 cd _site
 if [ ! -d .git ]; then
     git init
+
+    # reuse local checkout's objects if possible
+    alternate_store=$(pwd)/../../aleph_null/.git/objects
+    if [ ! -d ${alternate_store} ]; then
+        echo "WARNING: '$alternate_store' checkout does not exist"
+        echo "WARNING: push will always refetch initial state."
+    else
+        echo "$alternate_store" >> .git/objects/info/alternates
+    fi
+
     git checkout --orphan gh-pages
     git remote add --fetch origin git@github.com:skvadrik/aleph_null.git
 fi
