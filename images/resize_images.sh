a4fb19faba7b148dad8405bd7cd97dd381ef7683
Ulya Trofimovich <skvadrik@gmail.com>
2022-08-03 07:44:37 +0100

    New post about Helvellyn.

--- images/resize_images.sh
+++ images/resize_images.sh
@@ -8,8 +8,7 @@ fi
 
 for f in "$1"/*
 do
-    # convert -resize 1200x900 $f $f
-    convert -resize 1000x750 $f $f
+    convert -resize 1000x750 -unsharp 0x0.5+1.0+0 -quality 100% $f $f
     # convert -resize 800x600 $f $f
     # convert -resize 720x540 $f $f
 done
